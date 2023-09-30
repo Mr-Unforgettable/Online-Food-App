87aeb6ab3109fb35f1e0a23cf5c83166827828c6
Mr-Unforgettable <rulzabhinav69@gmail.com>
2023-09-30 14:58:36 +0530

    Code Cleanup

--- backend/routes/createUser.js
+++ backend/routes/createUser.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const router = express.Router()
 const User = require('../models/User')
+const router = express.Router()
 
 router.post('/createUser', async (req, res) => {
   try {
