9bd2b8306ba4282e874c72c6ab3258841f27662d
Mr-Unforgettable <rulzabhinav69@gmail.com>
2023-10-11 16:31:42 +0530

    Fixed the issue with myOrder page, endpoint.

--- src/screens/MyOrder.jsx
+++ src/screens/MyOrder.jsx
@@ -3,31 +3,20 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 export default function MyOrder () {
-  const [orderData, setOrderData] = useState([])
-
+  const [orderData, setorderData] = useState({})
   const fetchMyOrder = async () => {
-    const userEmail = localStorage.getItem('userEmail')
-    if (!userEmail) return
-
-    try {
-      const response = await fetch('http://localhost:3001/api/myOrder', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: userEmail
-        })
+    await fetch('http://localhost:3001/api/myOrder', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: localStorage.getItem('userEmail')
       })
-
-      if (response.ok) {
-        const data = await response.json()
-        console.log(data)
-        setOrderData(data)
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error)
-    }
+    }).then(async (res) => {
+      const response = await res.json()
+      setorderData(response)
+    })
   }
 
   useEffect(() => {
@@ -35,58 +24,80 @@ export default function MyOrder () {
   }, [])
 
   return (
-    <>
-      <Navbar />
+    <div>
+      <div>
+        <Navbar />
+      </div>
 
       <div className="container">
-        {Array.isArray(orderData) &&
-          orderData[0].orderData.order_data.map((order, index) => {
-            const orderItems = order[0]
-            const orderDate = order[1]
-
-            return (
-              <div key={index}>
-                <div className="m-auto mt-5">
-                  {orderDate}
-                  <hr />
-                </div>
-
-                {orderItems.map((item, itemIndex) => {
-                  return (
-                    <div className="col-12 col-md-6 col-lg-3" key={itemIndex}>
-                      <div className="card mt-3">
-                        <img
-                          src={item.img}
-                          className="card-img-top"
-                          alt="..."
-                          style={{ height: '120px', objectFit: 'fill' }}
-                        />
-
-                        <div className="card-body">
-                          <h5 className="card-title">{item.name}</h5>
-
-                          <div
-                            className="container w-100 p-0"
-                            style={{ height: '38px' }}
-                          >
-                            <span className="m-1">{item.qty}</span>
-                            <span className="m-1">{item.size}</span>
-                            <span className="m-1">{orderDate}</span>
-                            <div className="d-inline ms-2 h-100 w-20 fs-5">
-                              ₹{item.price}/-
+        <div className="row">
+          {orderData !== {}
+            ? Array(orderData).map((data) => {
+              return data.orderData
+                ? data.orderData.order_data
+                  .slice(0)
+                  .reverse()
+                  .map((item) => {
+                    return item.map((arrayData) => {
+                      return (
+                            <div key={arrayData.order_date}>
+                              {arrayData.order_date
+                                ? (
+                                <div className="m-auto mt-5">
+                                  {(data = arrayData.order_date)}
+                                  <hr />
+                                </div>
+                                  )
+                                : (
+                                <div className="col-12 col-md-6 col-lg-3">
+                                  <div
+                                    className="card mt-3"
+                                    style={{
+                                      width: '16rem',
+                                      maxHeight: '360px'
+                                    }}
+                                  >
+                                    <div className="card-body">
+                                      <h5 className="card-title">
+                                        {' '}
+                                        {arrayData.name}{' '}
+                                      </h5>{' '}
+                                      <div
+                                        className="container w-100 p-0"
+                                        style={{
+                                          height: '38px'
+                                        }}
+                                      >
+                                        <span className="m-1">
+                                          {' '}
+                                          {arrayData.qty}{' '}
+                                        </span>{' '}
+                                        <span className="m-1">
+                                          {' '}
+                                          {arrayData.size}{' '}
+                                        </span>{' '}
+                                        <span className="m-1"> {data} </span>{' '}
+                                        <div className=" d-inline ms-2 h-100 w-20 fs-5">
+                                          {' '}
+                                          ₹{arrayData.price}
+                                          /-{' '}
+                                        </div>{' '}
+                                      </div>{' '}
+                                    </div>{' '}
+                                  </div>
+                                </div>
+                                  )}
                             </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
+                      )
+                    })
+                  })
+                : ''
+            })
+            : ''}{' '}
+        </div>
       </div>
 
       <Footer />
-    </>
+    </div>
   )
 }
