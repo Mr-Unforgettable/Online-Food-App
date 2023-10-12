7ddcc96413412578965ce1c7560f6a2ba8781490
Mr-Unforgettable <rulzabhinav69@gmail.com>
2023-10-12 10:40:23 +0530

    Updated package.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import Login from './screens/Login'
 import Signup from './screens/Signup'
@@ -12,6 +9,20 @@ import MyOrder from './screens/MyOrder'
 import { CartProvider } from './components/ContextReducer'
 
 function App () {
+//  useEffect(() => {
+//    // Add an event listener to clear local storage on beforeunload
+//    const clearLocalStorage = () => {
+//      // Clear the local storage
+//      localStorage.clear()
+//    }
+//    window.addEventListener('beforeunload', clearLocalStorage)
+//
+//    // Clean up the event listenter when the component unmounts
+//    return () => {
+//      window.removeEventListener('beforeunload', clearLocalStorage)
+//    }
+//  }, [])
+
   return (
     <>
       <CartProvider>
